6f74517c5347917b5d599031780a59b9dcc64205
Whuers <752340690@qq.com>
2019-01-03 18:11:09 +0800

    change

--- src/onboardsdk/dji_sdk/dji_sdk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/indigo/setup.bash
-source /home/exbot/catkin_ws/devel/setup.bash
-
-roslaunch dji_sdk sdk_manifold.launch
--- src/onboardsdk/dji_sdk_demo/dji_sdk_demo_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/indigo/setup.bash
-source /home/exbot/catkin_ws/devel/setup.bash
-
-roslaunch dji_sdk_demo dji_sdk_client.launch
